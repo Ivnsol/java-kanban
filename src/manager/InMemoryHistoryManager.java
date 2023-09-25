a10ab7d989a411b3b650e4681c322d6919f96a1c
John_SL <vansol9797@yandex.ru>
2023-09-25 13:31:16 +0300

    refactor: теперь обращение к закрытым полям ноды, ограничен доступ к полям

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,90 +1,91 @@
 package manager;
 
-import model.Task;
 import Node.Node;
+import model.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    CustomLinkedLists his = new CustomLinkedLists();
+    private final CustomLinkedLists history = new CustomLinkedLists();
 
     //private static final int HISTORY_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        his.linkLast(task);
+        history.linkLast(task);
     }
 
     @Override
     public void remove(int id) {
-        if (his.nodeHistory.containsKey(id)) {
-            his.removeNode(his.nodeHistory.get(id));
-            his.nodeHistory.remove(id);
+        if (history.nodeHistory.containsKey(id)) {
+            history.removeNode(history.nodeHistory.get(id));
+            history.nodeHistory.remove(id);
+        } else {
+            System.out.println("Такой задачи нет в списке"); //проверка на ошибку
         }
     }
 
     @Override
-    public ArrayList<Task> getHistory() {
-        return his.getTasks();
+    public List<Task> getHistory() {
+        return history.getTasks();
     }
 
 
+    protected static class CustomLinkedLists {
+        protected Node last;
+        protected Node first;
+        protected HashMap<Integer, Node> nodeHistory = new HashMap<>();
+
+        protected CustomLinkedLists() {
+            last = null;
+            first = null;
+        }
+
+        protected void removeNode(final Node node) {
+            Node prev = node.getPrev();
+            Node next = node.getNext();
+
+            if (prev == null) {
+                first = next;
+            } else {
+                prev.setNext(next);
+            }
+
+            if (next == null) {
+                last = prev;
+            } else {
+                next.setPrev(prev);
+            }
+        }
+
+        protected void linkLast(Task task) {
+            Node l = last;
+            final Node newNode = new Node(l, task, null);
+            if (nodeHistory.containsKey(task.getId())) {
+                removeNode(nodeHistory.get(task.getId()));
+            }
+            last = newNode;
+            if (l == null) {
+                first = newNode;
+            } else {
+                l.setNext(newNode);
+                newNode.setPrev(l);
+            }
+            nodeHistory.put(task.getId(), newNode);
+        }
+
+
+        protected List<Task> getTasks() {
+            List<Task> tasks = new ArrayList<>();
+            Node node = first;
+            while (node != null) {
+                tasks.add(node.getItem());
+                node = node.getNext();
+            }
+            return tasks;
+        }
+    }
 }
-
-  class CustomLinkedLists {
-      protected Node last;
-      protected Node first;
-
-    protected CustomLinkedLists() {
-        last = null;
-        first = null;
-    }
-      HashMap<Integer, Node> nodeHistory = new HashMap<>();
-      protected void removeNode(final Node node) {//(предыдущий элемент, task, след элемент)
-            Node prev = node.prev; // предыдущий элемент = прев.
-            Node next = node.next; // след элеменет = след.
-
-          if (prev == null) {
-              first = next;
-          } else {
-              prev.next = next;
-          }
-
-          if (next == null) {
-              last = prev;
-          } else {
-              next.prev = prev;
-          }
-      }
-
-      protected void linkLast(Task task) {
-          Node l = last;
-          final Node newNode = new Node(l, task, null);
-          if (nodeHistory.containsKey(task.getId())) {
-              removeNode(nodeHistory.get(task.getId()));
-          }
-              last = newNode;
-              if (l == null) {
-                  first = newNode;
-              } else {
-                  l.next = newNode;
-                  newNode.prev = l;
-              }
-              nodeHistory.put(task.getId(), newNode);
-      }
-
-
-
-      protected ArrayList<Task> getTasks() {
-          ArrayList<Task> tasks = new ArrayList<>();
-          Node node = first;
-          while (node != null) {
-              tasks.add(node.item);
-              node = node.next;
-          }
-          return tasks;
-   }
-
-  }
