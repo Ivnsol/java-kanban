03d9ae790a14f9896ca6852f47647f818d31fabb
John_SL <vansol9797@yandex.ru>
2023-10-07 20:34:37 +0300

    refactor: getDefault убран historyManager из конструктора

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,16 +1,27 @@
 package manager;
 
+import java.io.File;
+
 public class Managers {
     public static TaskManager getDefault() {
 
-        HistoryManager historyManager = getDefaultHistory();
-
-        return new InMemoryTaskManager(historyManager);
+        return new InMemoryTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
 
         return new InMemoryHistoryManager();
     }
+    public static FileBackedTasksManager loadFromFile(File file){
+        FileBackedTasksManager manager = new FileBackedTasksManager(file);
+        manager.fileReader();
+        return manager;
+    }
+//    public static TaskManager getDefaultFileManager() {
+//        HistoryManager historyManager = getDefaultHistory();
+//        File file = new File();
+//        return new FileBackedTasksManager(historyManager);
+//    }
+
 }
 
