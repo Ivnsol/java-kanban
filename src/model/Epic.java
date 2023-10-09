eb3ee3ef1e050f2c2aa70ea288210952f72f35d1
John_SL <vansol9797@yandex.ru>
2023-10-09 18:53:31 +0300

    refactor: изменено поле type

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -10,7 +10,7 @@ public class Epic extends Task {
 
     public Epic(String title, String description) {
         super(title, description);
-        this.type = Types.EPIC;
+        super.type = Types.EPIC;
     }
 
     @Override
