c79e49a92eb3da12ce68d6ebf7efa7bdee9facc5
John_SL <vansol9797@yandex.ru>
2023-10-09 12:53:39 +0300

    refactor: getDefault

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -5,19 +5,14 @@ import java.io.File;
 public class Managers {
     public static TaskManager getDefault() {
         File file = new File("example.txt");
-        InMemoryTaskManager memory = loadFromFile(file);
-        return memory;
+        return new FileBackedTasksManager(file);
     }
 
     public static HistoryManager getDefaultHistory() {
 
         return new InMemoryHistoryManager();
     }
-    public static FileBackedTasksManager loadFromFile(File file){
-        FileBackedTasksManager manager = new FileBackedTasksManager(file);
-        manager.fileReader();
-        return manager;
-    }
+
 
 }
 
