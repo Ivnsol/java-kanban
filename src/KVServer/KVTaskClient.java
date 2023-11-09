aeaf8906d8618cc9cfd5bd00ba8936e462bf3ba2
John_SL <vansol9797@yandex.ru>
2023-11-09 22:32:20 +0300

    удалены лишние методы и добавлен apiToken

--- src/KVServer/KVTaskClient.java
+++ src/KVServer/KVTaskClient.java
@@ -3,6 +3,7 @@ package KVServer;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -10,44 +11,37 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+import static jdk.internal.util.xml.XMLStreamWriter.DEFAULT_CHARSET;
+
 public class KVTaskClient {
     private final String apiUrl;
-    private final String apiToken;
     Gson gson = new Gson();
 
-    public KVTaskClient(String apiUrl) {
+    public KVTaskClient(String apiUrl) throws IOException {
         this.apiUrl = apiUrl;
-        this.apiToken = register();
     }
 
-    private String register() {
-        try {
-            URL registerUrl = new URL(apiUrl + "/register");
-            HttpURLConnection connection = (HttpURLConnection) registerUrl.openConnection();
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode == 200) {
-                Scanner scanner = new Scanner(connection.getInputStream());
-                if (scanner.hasNext()) {
-                    return scanner.next();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+    private String apiToken() throws IOException {
+        URL url = new URL("http://localhost:8078/register");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = connection.getInputStream();
+        String body = new String(inputStream.readAllBytes(), DEFAULT_CHARSET);
+        return body;
     }
 
     public void put(String key, String json) {
         try {
-            URL saveURL = new URL(apiUrl + "/save/" + key + "?API_TOKEN=" + apiToken);
+            System.out.println(apiToken());
+            URL saveURL = new URL(apiUrl + "/save/" + key + "?API_TOKEN=" + apiToken());
             HttpURLConnection connection = (HttpURLConnection) saveURL.openConnection();
             connection.setRequestMethod("POST");
             connection.setDoOutput(true); // Устанавливаем разрешение записи данных в запрос
 
+            System.out.println("apiUrl: " + apiUrl);
+            System.out.println("saveURL: " + saveURL);
+
             try (OutputStream os = connection.getOutputStream()) {
+                System.out.println("Sending data: " + json);
                 os.write(json.getBytes(StandardCharsets.UTF_8));
             }
 
@@ -62,7 +56,7 @@ public class KVTaskClient {
 
     public String load(String key){
         try {
-            URL saveURL = new URL(apiUrl + "/load/" + key + "?API_TOKEN=" + apiToken);
+            URL saveURL = new URL(apiUrl + "/load/" + key + "?API_TOKEN=" + apiToken());
             HttpURLConnection connection = (HttpURLConnection) saveURL.openConnection();
             connection.setRequestMethod("GET");
 
@@ -79,7 +73,4 @@ public class KVTaskClient {
         }
     }
 
-    public String getApiToken() {
-        return apiToken;
-    }
 }
