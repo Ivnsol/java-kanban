9e413558a393d832f5fd4fb5335074bf09fee7e6
John_SL <vansol9797@yandex.ru>
2023-11-11 08:40:25 +0300

    remake logic

--- src/test/HttpTaskManagerTest.java
+++ src/test/HttpTaskManagerTest.java
@@ -2,35 +2,32 @@ package test;
 
 import KVServer.KVServer;
 import manager.HttpTaskManager;
+import manager.Managers;
 import manager.TaskManager;
 import model.Epic;
 import model.SubTask;
 import model.Task;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
-public class HttpTaskManagerTest {
-    TaskManager taskManager;
-    public KVServer kvServer = new KVServer();
-
-    public HttpTaskManagerTest() throws IOException {
-
-        this.taskManager = new HttpTaskManager("http://localhost:8078");
-        // Инициализация объекта taskManager
+public class HttpTaskManagerTest extends TaskManagerTest<HttpTaskManager> {
+KVServer kvServer;
+    {
+        try {
+            kvServer = new KVServer();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    @Override
     @BeforeEach
     public void init() {
         kvServer.start();
+        taskManager = (HttpTaskManager) Managers.getDefault();
         Epic epic1 = new Epic("epicName1", "epicDis1",null,0);
         taskManager.addEpic(epic1);
         Epic epic2 = new Epic("epicName2", "epicDis2",null,0);
@@ -48,258 +45,7 @@ public class HttpTaskManagerTest {
     }
 
     @AfterEach
-    public void tearDown() {
+    public void stop() {
         kvServer.stop();
     }
-    @Test
-    void getAllEpicTest() {
-        Collection<Epic> epicList = taskManager.getAllEpic();
-        assertEquals(2, epicList.size());
-
-    }
-
-    @Test
-    void getAllTaskTest() {
-        Collection<Task> tasks = taskManager.getAllTask();
-        assertEquals(2, tasks.size());
-    }
-
-    @Test
-    void getAllSubTaskTest() {
-        Collection<SubTask> subTasks = taskManager.getAllSubTask();
-        assertEquals(3, subTasks.size());
-    }
-
-    @Test
-    void removeAllTasksForEpicTest() {
-        taskManager.removeAllTasksForEpic();
-        Collection<Epic> epicList = taskManager.getAllEpic();
-        assertTrue(epicList.isEmpty());
-    }
-
-    @Test
-    void removeAllTaskTest() {
-        taskManager.removeAllTask();
-        Collection<Task> tasks = taskManager.getAllTask();
-        assertTrue(tasks.isEmpty());
-    }
-
-    @Test
-    void removeAllSubTaskTest() {
-        taskManager.removeAllSubTask();
-        Collection<SubTask> subTasks = taskManager.getAllSubTask();
-        assertTrue(subTasks.isEmpty());
-    }
-
-    @Test
-    void removeEpicByIdTest() {
-        //find epic id1 and check Collections before delete
-        Collection<SubTask> subTasksBeforeDelete = taskManager.getAllSubTask();
-        Collection<Epic> epicsBeforeDelete = taskManager.getAllEpic();
-        Optional<Epic> epic = Optional.ofNullable((Epic) taskManager.getTaskByIdForEpic(1));
-        assertEquals(3, subTasksBeforeDelete.size());
-        assertEquals(2, epicsBeforeDelete.size());
-        assertTrue(epic.isPresent());
-
-        //throws NullPointerException after delete and check collections
-        taskManager.removeEpicById(1);
-
-        assertThrows(NullPointerException.class,
-                () -> taskManager.getTaskByIdForEpic(1));
-
-        Collection<SubTask> subTasks = taskManager.getAllSubTask();
-        Collection<Epic> epics = taskManager.getAllEpic();
-        assertEquals(0, subTasks.size());
-        assertEquals(1, epics.size());
-    }
-
-    @Test
-    void removeTaskByIdTest() {
-        taskManager.removeTaskById(3);
-        Collection<Task> tasks = taskManager.getAllTask();
-        assertEquals(1, tasks.size());
-    }
-
-    @Test
-    void removeSubtaskIdTest() {
-        taskManager.removeSubtaskId(6);
-        Collection<SubTask> subTasks = taskManager.getAllSubTask();
-        assertEquals(2, subTasks.size());
-    }
-
-    @Test
-    void getTaskByIdForEpicTest() {
-        taskManager.getAllEpic();
-        Epic epic = (Epic) taskManager.getTaskByIdForEpic(1);
-        assertEquals("1, EPIC, epicName1, NEW, epicDis1, null, 0",//may be null at the end
-                epic.toString());
-    }
-
-    @Test
-    void getTaskByIdForTaskTest() {
-        assertEquals("3, TASK, taskName3, NEW, taskDis3, 2023.10.23 15:10, 0",//may be null at the end
-                taskManager.getTaskByIdForTask(3).toString());
-    }
-
-    @Test
-    void getTaskByIdForSubTaskTest() {
-        assertEquals("3, TASK, taskName3, NEW, taskDis3, 2023.10.23 15:10, 0",//may be null at the end
-                taskManager.getTaskByIdForTask(3).toString());
-    }
-
-    @Test
-    void addEpicTest() {
-        Epic epic3 = new Epic("epicName3", "epicDis3",null,0);
-        taskManager.addEpic(epic3);
-        assertEquals(3, taskManager.getAllEpic().size());
-    }
-
-    @Test
-    void addSubTaskTest() {
-        SubTask subTask8 = new SubTask("subTaskName8", "subTaskDis", 1, LocalDateTime.of(2023, 11, 23, 15, 14), 0);
-
-        taskManager.addSubTask(subTask8);
-        assertEquals(4, taskManager.getAllSubTask().size());
-    }
-
-    @Test
-    void addTaskTest() {
-        Task task4 = new Task("taskName4", "taskDis4", LocalDateTime.of(2023, 10, 23, 16, 10), 0);
-
-        taskManager.addTask(task4);
-        assertEquals(3, taskManager.getAllTask().size());
-    }
-
-    @Test
-    void updateEpicWithAllNewStatusSubTasksTest() {
-        Epic epic = (Epic) taskManager.getTaskByIdForEpic(1);
-        taskManager.updateEpic(epic);
-        assertEquals("NEW", epic.getStatus());
-    }
-
-    @Test
-    void updateEpicWithDoneAndNewStatusSubTasksTest() {
-        Epic epic = (Epic) taskManager.getTaskByIdForEpic(1);
-
-        SubTask sub8 = new SubTask("sub8", "subTaskDis", 1, LocalDateTime.of(2023, 10, 24, 15, 13), 0);
-        sub8.setStatus("DONE");
-        taskManager.addSubTask(sub8);
-
-        SubTask sub9 = new SubTask("sub9", "subTaskDis", 1, LocalDateTime.of(2023, 10, 25, 15, 14), 0);
-        sub9.setStatus("DONE");
-        taskManager.addSubTask(sub9);
-
-        taskManager.updateEpic(epic);
-        assertEquals("IN_PROGRESS", epic.getStatus());
-    }
-
-    @Test
-    void updateEpicWithAllDoneStatusSubTasksTest() {
-        Epic epic = (Epic) taskManager.getTaskByIdForEpic(1);
-
-        taskManager.removeAllSubTask();
-
-        SubTask newSub8StatusDone = new SubTask("sub8", "subTaskDis", 1, LocalDateTime.of(2023, 10, 24, 15, 13), 0);
-        newSub8StatusDone.setStatus("DONE");
-        taskManager.addSubTask(newSub8StatusDone);
-
-        SubTask newSub9StatusDone = new SubTask("sub9", "subTaskDis", 1, LocalDateTime.of(2023, 10, 25, 15, 14), 0);
-        newSub9StatusDone.setStatus("DONE");
-        taskManager.addSubTask(newSub9StatusDone);
-
-        taskManager.updateEpic(epic);
-        assertEquals("DONE", epic.getStatus());
-    }
-
-    @Test
-    void updateEpicWithAllInProgressStatusSubTasksTest() {
-        Epic epic = (Epic) taskManager.getTaskByIdForEpic(1);
-
-        taskManager.removeAllSubTask();
-
-        SubTask newSub8StatusInProgress = new SubTask("sub8", "subTaskDis", 1, LocalDateTime.of(2023, 10, 24, 15, 13), 0);
-        newSub8StatusInProgress.setStatus("IN_PROGRESS");
-        taskManager.addSubTask(newSub8StatusInProgress);
-
-        SubTask newSub9StatusInProgress = new SubTask("sub9", "subTaskDis", 1, LocalDateTime.of(2023, 10, 25, 15, 14), 0);
-        newSub9StatusInProgress.setStatus("IN_PROGRESS");
-        taskManager.addSubTask(newSub9StatusInProgress);
-
-        taskManager.updateEpic(epic);
-        assertEquals("IN_PROGRESS", epic.getStatus());
-    }
-
-    @Test
-    void updateEpicWithNoSubTasksTest() {
-        Epic epic = (Epic) taskManager.getTaskByIdForEpic(1);
-
-        taskManager.removeAllSubTask();
-
-        taskManager.updateEpic(epic);
-        assertEquals("NEW", epic.getStatus());
-    }
-
-    @Test
-    void updateTaskTest() {
-        Task task3 = new Task("taskName3", "taskDis3", LocalDateTime.of(2024, 10, 23, 15, 10), 0);
-        // Вызов метода updateTask
-        taskManager.addTask(task3);
-        taskManager.updateTask(task3);
-
-        // Проверка ожидаемого результата
-        Task updatedTask = taskManager.getTaskByIdForTask(8);
-        assertEquals(task3, updatedTask);
-    }
-
-    @Test
-    void updateSubTaskTest() {
-        SubTask subTask5 = new SubTask("subTaskName5", "subTaskDis", 1, LocalDateTime.of(2024, 10, 23, 15, 12), 0);
-        // Вызов метода updateTask
-        taskManager.addSubTask(subTask5);
-        taskManager.updateTask(subTask5);
-
-        // Проверка ожидаемого результата
-        Task updatedTask = taskManager.getTaskByIdForSubTask(8);
-        assertEquals(subTask5, updatedTask);
-    }
-
-    @Test
-    void CheckEpicForSubtaskTest() {
-        SubTask subTask5 = new SubTask("subTaskName5", "subTaskDis", 1, LocalDateTime.of(2024, 10, 23, 15, 12), 0);
-        // Вызов метода updateTask
-        taskManager.addSubTask(subTask5);
-
-        // Проверка ожидаемого результата
-        SubTask updatedTask = (SubTask) taskManager.getTaskByIdForSubTask(8);
-        assertEquals(1, updatedTask.getEpicId());
-    }
-
-    @Test
-    void getPrioritizedTasksTest() {
-        SubTask subTask5 = new SubTask("subTaskName5", "subTaskDis", 1, null, 0);
-        taskManager.addSubTask(subTask5);
-
-        SubTask subTask9 = new SubTask("subTaskName555", "subTaskDis", 1, null, 0);
-        taskManager.addSubTask(subTask9);
-
-        SubTask subTask10 = new SubTask("222", "subTaskDis", 1, null, 0);
-        taskManager.addSubTask(subTask10);
-
-        System.out.println(taskManager.getPrioritizedTasks());
-        assertEquals(8, taskManager.getPrioritizedTasks().size());
-
-    }
-
-    @Test
-    void getTasksForEpicTest() {
-        taskManager.getTasksForEpic(1);
-        assertEquals(3, taskManager.getTasksForEpic(1).size());
-    }
-
-    @Test
-    void printHistoryTest() {
-        List<Task> history = taskManager.printHistory();
-        assertEquals(0, history.size());
-    }
-
 }
